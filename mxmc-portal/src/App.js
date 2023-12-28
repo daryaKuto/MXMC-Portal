67c7763a2a73badcbcef339978c6f8049fe57eb0
Darya Kutovaya <kutovayadarya@gmail.com>
2023-12-28 15:09:03 -0800

    rough draft of the layout

--- mxmc-portal/src/App.js
+++ mxmc-portal/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; // Make sure your App.css has the styles for the layout
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        {/* Placeholder for header video/photo upload */}
+        <button className="upload-btn">+</button>
+        <p>HEADER VIDEO / PHOTO UPLOAD</p>
+      </div>
+      <div className="profile-section">
+        {/* Placeholder for profile video/photo */}
+        <div className="profile-upload">
+          <button className="upload-btn">+</button>
+          <p>PROFILE VIDEO / PHOTO</p>
+        </div>
+        <div className="additional-photos">
+          {/* Placeholder for additional photos */}
+          <button className="upload-btn">+</button>
+          <button className="upload-btn">+</button>
+        </div>
+      </div>
+      <div className="user-info">
+        <input type="text" placeholder="Milana Kijajic" />
+        <select>
+          <option value="female">Female</option>
+          {/* ... other options ... */}
+        </select>
+        <input type="text" placeholder="Vancouver, BC" />
+        <input type="date" defaultValue="2000-01-01" />
+      </div>
+      <div className="roles">
+        {/* Checkboxes for user roles */}
+        <label><input type="checkbox" /> Content Creator</label>
+        <label><input type="checkbox" /> Influencer</label>
+        <label><input type="checkbox" /> Model</label>
+        <label><input type="checkbox" /> Athlete</label>
+        {/* ... other roles ... */}
+      </div>
+      <textarea className="about-you" placeholder="Tell us a little about you..."></textarea>
     </div>
   );
 }
